8364c0a8413f836fde26d96c8de2d18442e52a15
Francisco Morillas <francisco.morillas@upc.edu>
2026-04-24 17:18:39 +0200

    = fora el _site

--- _site/en-post-claustre.sh
+++
@@ -1,1 +0,0 @@
-git add _posts/2026-04-22-Claustre-Elections.md ; git commit -m"Elections Claustre" ; git push
--- _site/post-claustre.sh
+++
@@ -1,1 +0,0 @@
-git add _posts/2026-04-23-Eleccions-Claustre.md ; git commit -m"Eleccions Claustre" ; git push
